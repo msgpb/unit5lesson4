aa0b92383ad2541ebb392cde8939571ccc1ac09b
Mira Biswas <mbiswas2025@kehillahstudent.org>
2023-05-04 11:19:38 -0700

    q1

---
+++ q1.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+url = "https://en.wikipedia.org/wiki/Minnesota"
+dfs = pd.read_html(
+    url,
+    match = "Historical population"
+)
+
+df = dfs[0]
+df = df.iloc[0:-2]
+df = df[["Census","Pop."]].astype("int")
+print(df.info())
+df.plot(x = "Census", y = "Pop.")
+df.plot()
+plt.show()
